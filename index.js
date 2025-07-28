536f7edd19d0f012d696db7165a6a75b72426a56
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-07-28 22:14:30 +0300

    feat: add read or not read to card

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const cards = document.querySelector(".cards");
 
 const library = [];
 
-function Book(name, author, pages) {
+function Book(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.id = crypto.randomUUID();
 }
 
-function addBooksToLibrary(name, author, pages) {
-    const book = new Book(name, author, pages);
+function addBooksToLibrary(name, author, pages, read) {
+    const book = new Book(name, author, pages, read);
     library.push(book);
 }
 
@@ -20,22 +21,25 @@ function displayBooks() {
         const bookTitleEL = document.createElement("h1"); 
         const bookAuthorEL = document.createElement("h3");
         const bookPagesEL = document.createElement("h4");
+        const readEL = document.createElement("h4");
         bookTitleEL.innerText = book.name;
         bookAuthorEL.innerText = `By: ${book.author}`;
         bookPagesEL.innerText = `has ${book.pages} pages.`;
-        bookEL.classList.add("card")
+        readEL.innerText = `Read: ${book.read}`;
+        bookEL.classList.add("card");
         bookEL.appendChild(bookTitleEL);
         bookEL.appendChild(bookAuthorEL);
         bookEL.appendChild(bookPagesEL);
+        bookEL.appendChild(readEL);
         cards.appendChild(bookEL);
     });
 }
 
-addBooksToLibrary("meow", "aaa", 329);
-addBooksToLibrary("meowy", "bbb", 330);
-addBooksToLibrary("woof", "ccc", 331);
-addBooksToLibrary("meow", "aaa", 329);
-addBooksToLibrary("meowy", "bbb", 330);
-addBooksToLibrary("woof", "ccc", 331);
+addBooksToLibrary("meow", "aaa", 329, "no");
+addBooksToLibrary("meowy", "bbb", 330, "yes");
+addBooksToLibrary("woof", "ccc", 331, "no");
+addBooksToLibrary("meow", "aaa", 329, "no");
+addBooksToLibrary("meowy", "bbb", 330, "yes");
+addBooksToLibrary("woof", "ccc", 331, "yes");
 
 displayBooks();

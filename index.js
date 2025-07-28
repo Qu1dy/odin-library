9dbd10c0bfcf7a947143dcd11dd0814b54833b63
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-07-28 20:08:17 +0300

    feat: add addBooksToLibrary function

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function Book(name, author, pages) {
     this.pages = pages;
     this.id = crypto.randomUUID();
 }
+
+function addBooksToLibrary(name, author, pages) {
+    const book = new Book(name, author, pages);
+    library.push(book);
+}

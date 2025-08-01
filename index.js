72092ce289102c99a5936f44a660400cc8dcacae
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-01 10:57:37 +0300

    fix: all books were added instead of only the new

--- index.js
+++ index.js
@@ -17,27 +17,26 @@ function Book(name, author, pages, read) {
 function addBooksToLibrary(name, author, pages, read) {
     const book = new Book(name, author, pages, read);
     library.push(book);
+    displayBook(book);
 }
 
 
-function displayBooks() {
-    library.forEach(book => {
-        const bookEL = document.createElement("div");
-        const bookTitleEL = document.createElement("h1"); 
-        const bookAuthorEL = document.createElement("h3");
-        const bookPagesEL = document.createElement("h4");
-        const readEL = document.createElement("h4");
-        bookTitleEL.innerText = book.name;
-        bookAuthorEL.innerText = `By: ${book.author}`;
-        bookPagesEL.innerText = `has ${book.pages} pages.`;
-        readEL.innerText = `Read: ${book.read}`;
-        bookEL.classList.add("card");
-        bookEL.appendChild(bookTitleEL);
-        bookEL.appendChild(bookAuthorEL);
-        bookEL.appendChild(bookPagesEL);
-        bookEL.appendChild(readEL);
-        cards.appendChild(bookEL);
-    });
+function displayBook(book) {
+    const bookEL = document.createElement("div");
+    const bookTitleEL = document.createElement("h1"); 
+    const bookAuthorEL = document.createElement("h3");
+    const bookPagesEL = document.createElement("h4");
+    const readEL = document.createElement("h4");
+    bookTitleEL.innerText = book.name;
+    bookAuthorEL.innerText = `By: ${book.author}`;
+    bookPagesEL.innerText = `has ${book.pages} pages.`;
+    readEL.innerText = `Read: ${book.read}`;
+    bookEL.classList.add("card");
+    bookEL.appendChild(bookTitleEL);
+    bookEL.appendChild(bookAuthorEL);
+    bookEL.appendChild(bookPagesEL);
+    bookEL.appendChild(readEL);
+    cards.appendChild(bookEL);
 }
 
 addBookEL.addEventListener("click", () => dialog.showModal());

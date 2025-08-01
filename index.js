18064426649583801db8f367288ad5f86a75f2e6
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-01 13:20:25 +0300

    feat: make switch change read status

--- index.js
+++ index.js
@@ -14,56 +14,63 @@ function Book(name, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
+Book.prototype.toggleRead = function(read) {
+    this.read = read ? "✓" : "✖";
+}
+
 function addBooksToLibrary(name, author, pages, read) {
     const book = new Book(name, author, pages, read);
     library.push(book);
-    updateLibrary();
+    updateLibrary(book);
 }
 
 
-function updateLibrary() {
-    cards.innerHTML = "";
-    library.forEach(book => {
-        const bookEL = document.createElement("div");
-        const bookTitleEL = document.createElement("h1"); 
-        const bookAuthorEL = document.createElement("h3");
-        const bookPagesEL = document.createElement("h4");
-        const readEL = document.createElement("h4");
-        const buttonsDivEL = document.createElement("div");
-        const removeBookEL = document.createElement("button");
-        const indOfBookInLibrary = library.find(b => b.id === book.id)
-        removeBookEL.addEventListener("click", () => {
-            library.splice(indOfBookInLibrary, 1);
-            updateLibrary();
-        });
-        const xMark = document.createElement("img");
-        xMark.setAttribute("src", "svgs/close.svg");
-        xMark.setAttribute("title", "Remove book");
-        removeBookEL.appendChild(xMark);
-
-        const changeReadStatus = document.createElement("label");
-        changeReadStatus.classList.add("switch");
-        const readStatusCheckbox = document.createElement('input');
-        readStatusCheckbox.setAttribute("type", "checkbox");
-        const readStatusSlider = document.createElement("span");
-        readStatusSlider.classList.add("slider");
-        changeReadStatus.appendChild(readStatusCheckbox);
-        changeReadStatus.appendChild(readStatusSlider);
-        buttonsDivEL.appendChild(changeReadStatus);
-
-        buttonsDivEL.appendChild(removeBookEL);
-        bookTitleEL.innerText = book.name;
-        bookAuthorEL.innerText = `By: ${book.author}`;
-        bookPagesEL.innerText = `has ${book.pages} pages.`;
-        readEL.innerText = `Read: ${book.read}`;
-        bookEL.classList.add("card");
-        bookEL.appendChild(bookTitleEL);
-        bookEL.appendChild(bookAuthorEL);
-        bookEL.appendChild(bookPagesEL);
-        bookEL.appendChild(readEL);
-        bookEL.appendChild(buttonsDivEL);
-        cards.appendChild(bookEL);
+function updateLibrary(book) {
+    const bookEL = document.createElement("div");
+    const bookTitleEL = document.createElement("h1"); 
+    const bookAuthorEL = document.createElement("h3");
+    const bookPagesEL = document.createElement("h4");
+    const readEL = document.createElement("h4");
+    readEL.classList.add("read");
+    const buttonsDivEL = document.createElement("div");
+    const removeBookEL = document.createElement("button");
+    const indOfBookInLibrary = library.find(b => b.id === book.id)
+    removeBookEL.addEventListener("click", () => {
+        library.splice(indOfBookInLibrary, 1);
+        updateLibrary();
     });
+    const xMark = document.createElement("img");
+    xMark.setAttribute("src", "svgs/close.svg");
+    xMark.setAttribute("title", "Remove book");
+    removeBookEL.appendChild(xMark);
+
+    const changeReadStatus = document.createElement("label");
+    changeReadStatus.classList.add("switch");
+    const readStatusCheckbox = document.createElement('input');
+    readStatusCheckbox.setAttribute("type", "checkbox");
+    readStatusCheckbox.addEventListener("change", (e) => {
+        const checkbox = e.target;
+        book.toggleRead(checkbox.checked);
+        bookEL.querySelector(".read").textContent = `Read: ${book.read}`;
+    })
+    const readStatusSlider = document.createElement("span");
+    readStatusSlider.classList.add("slider");
+    changeReadStatus.appendChild(readStatusCheckbox);
+    changeReadStatus.appendChild(readStatusSlider);
+    buttonsDivEL.appendChild(changeReadStatus);
+
+    buttonsDivEL.appendChild(removeBookEL);
+    bookTitleEL.innerText = book.name;
+    bookAuthorEL.innerText = `By: ${book.author}`;
+    bookPagesEL.innerText = `has ${book.pages} pages.`;
+    readEL.innerText = `Read: ${book.read}`;
+    bookEL.classList.add("card");
+    bookEL.appendChild(bookTitleEL);
+    bookEL.appendChild(bookAuthorEL);
+    bookEL.appendChild(bookPagesEL);
+    bookEL.appendChild(readEL);
+    bookEL.appendChild(buttonsDivEL);
+    cards.appendChild(bookEL);
 }
 
 addBookEL.addEventListener("click", () => dialog.showModal());

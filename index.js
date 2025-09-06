e4ea3b6de357087f89f7ccf102f6d2ec1a4f0162
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-09-06 10:57:45 +0300

    feat: add form validation

--- index.js
+++ index.js
@@ -1,100 +1,122 @@
 const cards = document.querySelector(".cards-container");
-const addBookEL = document.querySelector(".add-book")
+const addBookEL = document.querySelector(".add-book");
 const dialog = document.querySelector("#add-book-dia");
 const overlay = document.querySelector(".overlay");
 const addBookForm = document.querySelector("form");
-const submitFormButton = document.querySelector("button[type=submit]")
+const submitFormButton = document.querySelector("button[type=submit]");
+const error = document.querySelector(".error");
 
 class Book {
-    constructor(name, author, pages, read) {
-        this.name = name;
-        this.author = author;
-        this.pages = pages;
-        this.read = read ? "✓" : "✖";
-    }
+  constructor(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read ? "✓" : "✖";
+  }
 
-    toggleRead(read) {
-        this.read = read ? "✓" : "✖";
-    }
+  toggleRead(read) {
+    this.read = read ? "✓" : "✖";
+  }
 }
 
 const addBooksToLibrary = (name, author, pages, read) => {
-    const book = new Book(name, author, pages, read);
-    updateLibrary(book);
-}
-
+  const book = new Book(name, author, pages, read);
+  updateLibrary(book);
+};
 
 const createReadStatusElement = (book, bookEL) => {
-    const changeReadStatus = document.createElement("label");
-    changeReadStatus.classList.add("switch");
-    const readStatusCheckbox = document.createElement('input');
-    readStatusCheckbox.setAttribute("type", "checkbox");
-    if(book.read === "✓") {
-        readStatusCheckbox.checked = true;
-    }
-    readStatusCheckbox.addEventListener("change", (e) => {
-        const checkbox = e.target;
-        book.toggleRead(checkbox.checked);
-        bookEL.querySelector(".read").textContent = `Read: ${book.read}`;
-    })
-    const readStatusSlider = document.createElement("span");
-    readStatusSlider.classList.add("slider");
-    changeReadStatus.appendChild(readStatusCheckbox);
-    changeReadStatus.appendChild(readStatusSlider);
-    return changeReadStatus;
-}
+  const changeReadStatus = document.createElement("label");
+  changeReadStatus.classList.add("switch");
+  const readStatusCheckbox = document.createElement("input");
+  readStatusCheckbox.setAttribute("type", "checkbox");
+  if (book.read === "✓") {
+    readStatusCheckbox.checked = true;
+  }
+  readStatusCheckbox.addEventListener("change", (e) => {
+    const checkbox = e.target;
+    book.toggleRead(checkbox.checked);
+    bookEL.querySelector(".read").textContent = `Read: ${book.read}`;
+  });
+  const readStatusSlider = document.createElement("span");
+  readStatusSlider.classList.add("slider");
+  changeReadStatus.appendChild(readStatusCheckbox);
+  changeReadStatus.appendChild(readStatusSlider);
+  return changeReadStatus;
+};
 
 const createRemoveBookElement = (bookEL) => {
-    const removeBookEL = document.createElement("button");
-    removeBookEL.addEventListener("click", () => {
-        bookEL.remove();
-    });
-    const xMark = document.createElement("img");
-    xMark.setAttribute("src", "svgs/close.svg");
-    xMark.setAttribute("title", "Remove book");
-    removeBookEL.appendChild(xMark);
-    return removeBookEL;
-}
+  const removeBookEL = document.createElement("button");
+  removeBookEL.addEventListener("click", () => {
+    bookEL.remove();
+  });
+  const xMark = document.createElement("img");
+  xMark.setAttribute("src", "svgs/close.svg");
+  xMark.setAttribute("title", "Remove book");
+  removeBookEL.appendChild(xMark);
+  return removeBookEL;
+};
 
 const updateLibrary = (book) => {
-    const bookEL = document.createElement("div");
-    const bookTitleEL = document.createElement("h1"); 
-    const bookAuthorEL = document.createElement("h3");
-    const bookPagesEL = document.createElement("h4");
-    const readEL = document.createElement("h4");
-    readEL.classList.add("read");
-    const buttonsDivEL = document.createElement("div");
-    const changeReadStatus = createReadStatusElement(book, bookEL);
-    buttonsDivEL.appendChild(changeReadStatus);
-    const removeBookEL = createRemoveBookElement(bookEL);
-    buttonsDivEL.appendChild(removeBookEL);
+  const bookEL = document.createElement("div");
+  const bookTitleEL = document.createElement("h1");
+  const bookAuthorEL = document.createElement("h3");
+  const bookPagesEL = document.createElement("h4");
+  const readEL = document.createElement("h4");
+  readEL.classList.add("read");
+  const buttonsDivEL = document.createElement("div");
+  const changeReadStatus = createReadStatusElement(book, bookEL);
+  buttonsDivEL.appendChild(changeReadStatus);
+  const removeBookEL = createRemoveBookElement(bookEL);
+  buttonsDivEL.appendChild(removeBookEL);
 
-    bookTitleEL.innerText = book.name;
-    bookAuthorEL.innerText = `By: ${book.author}`;
-    bookPagesEL.innerText = `has ${book.pages} pages.`;
-    readEL.innerText = `Read: ${book.read}`;
-    bookEL.classList.add("card");
+  bookTitleEL.innerText = book.name;
+  bookAuthorEL.innerText = `By: ${book.author}`;
+  bookPagesEL.innerText = `has ${book.pages} pages.`;
+  readEL.innerText = `Read: ${book.read}`;
+  bookEL.classList.add("card");
 
-    bookEL.appendChild(bookTitleEL);
-    bookEL.appendChild(bookAuthorEL);
-    bookEL.appendChild(bookPagesEL);
-    bookEL.appendChild(readEL);
-    bookEL.appendChild(buttonsDivEL);
-    cards.appendChild(bookEL);
-}
+  bookEL.appendChild(bookTitleEL);
+  bookEL.appendChild(bookAuthorEL);
+  bookEL.appendChild(bookPagesEL);
+  bookEL.appendChild(readEL);
+  bookEL.appendChild(buttonsDivEL);
+  cards.appendChild(bookEL);
+};
 
 addBookEL.addEventListener("click", () => dialog.showModal());
 
 dialog.addEventListener("toggle", (e) => {
-    if(e.newState === 'open')
-        return overlay.style.display = "block";
-    overlay.style.display = "none";
-})
+  if (e.newState === "open") return (overlay.style.display = "block");
+  overlay.style.display = "none";
+});
 
 submitFormButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const formElements = addBookForm.elements;
-    const [bookName,author,pages] = Array.from(formElements).map(item => item.value);
-    const read = formElements.item(3).checked;
-    addBooksToLibrary(bookName, author, pages, read);
+  e.preventDefault();
+  const formElements = addBookForm.elements;
+  const formChildren = addBookForm.children;
+  for (const child of Array.from(formChildren).filter(
+    (child) => child.tagName === "P"
+  )) {
+    const input = child.children[1];
+    const label = child.children[0];
+    if (!input.checkValidity()) {
+      error.textContent = `${input.validationMessage} (${
+        label.textContent.split(":")[0]
+      })`;
+
+      error.classList.add("active");
+      return;
+    }
+  }
+
+  error.textContent = "";
+  error.classList.remove("active");
+
+  const [bookName, author, pages] = Array.from(formElements).map(
+    (item) => item.value
+  );
+  const read = formElements.item(3).checked;
+
+  addBooksToLibrary(bookName, author, pages, read);
+  dialog.close();
 });
